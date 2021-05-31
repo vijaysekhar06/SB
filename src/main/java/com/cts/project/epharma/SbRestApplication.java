ec5cbc1f3d8a96d18cc1f692b790937a5bc13dff
vijaysekhar06 <45431749+vijaysekhar06@users.noreply.github.com>
2021-05-31 07:39:37 +0530

    Update SbRestApplication.java

--- src/main/java/com/cts/project/epharma/SbRestApplication.java
+++ src/main/java/com/cts/project/epharma/SbRestApplication.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SbRestApplication {
-	
+public class SbRestApplication {	
 	public static void main(String[] args) {
 		SpringApplication.run(SbRestApplication.class, args);
 	}
