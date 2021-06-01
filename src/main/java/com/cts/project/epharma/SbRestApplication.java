65766188a8410c5a130c01effd0981808931fa49
vijaysekhar06 <45431749+vijaysekhar06@users.noreply.github.com>
2021-06-01 16:31:42 +0530

    Update SbRestApplication.java

--- src/main/java/com/cts/project/epharma/SbRestApplication.java
+++ src/main/java/com/cts/project/epharma/SbRestApplication.java
@@ -18,7 +18,7 @@ public class SbRestApplication {
 	public String hello(@PathVariable("name") String name)
 	{
 
-		return "Hello Jenkins......."+name;
+		return ".....Hello Jenkins......."+name;
 	
 	}
 	
